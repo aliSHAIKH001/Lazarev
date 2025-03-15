f2367660940b37cd5e9042a76daf7ba5e26df560
Ali Shaikh <shahinshaikh603@gmail.com>
2025-03-15 19:54:09 +0530

    Created page 4 with some animation  affects

--- script.js
+++ script.js
@@ -98,3 +98,16 @@ function page3VideoAnimation() {
 }
 
 page3VideoAnimation();
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+            elem.childNodes[3].style.opacity = 1;
+            elem.childNodes[3].play();
+    })
+    elem.addEventListener("mouseleave", function(){
+        elem.childNodes[3].style.opacity = 0;
+        elem.childNodes[3].load();
+    })
+})
